fa36f3dc581e12ae9d0694a637c9544d47119373
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-02-14 13:15:59 -0600

    some early pca

--- pca_classifier.py
+++ pca_classifier.py
@@ -1,24 +1,19 @@
-
 from PIL import Image
 import numpy as np
+from sklearn.decomposition import PCA
+
 #import pandas as pd
 
-def compute_mean_image(images):
-    # print("len(images) {}".format(len(images)))
-    count = len(images)
+# def compute_mean_image(images):
+#     # print("len(images) {}".format(len(images)))
+#     count = len(images)
 
-
-    sum_of_images = np.zeros(3072)
-    # sum_of_images = [sum(x.T) for x in images]
-    for image in images:
-        sum_of_images +=image
-    # print("images[0] {}".format(images[0]))
-    # print("images[1] {}".format(images[1]))
-    # print("images[2] {}".format(images[2]))
-    # print("sum_of_images {}".format(sum_of_images))
-    mean_image = (sum_of_images/count).astype(int)
-    # return mean_image
-    return mean_image
+#     sum_of_images = np.zeros(3072)
+#     for image in images:
+#         sum_of_images +=image
+#     mean_image = (sum_of_images/count).astype(int)
+#     # return mean_image
+#     return mean_image
 
 def compute_princ_comps(images):
     pass
@@ -38,7 +33,7 @@ def unpickle(file):
 def get_class_images_from_files(files, class_id):
     images = []
     for file in files:
-        print("### Loading images of class {} from file {}".format(class_id, file))
+        # print("### Loading images of class {} from file {}".format(class_id, file))
         content = unpickle(file)
         labels= content[b'labels']
         img_data= content[b'data']
@@ -48,17 +43,14 @@ def get_class_images_from_files(files, class_id):
         img_data = img_data[mask]
 
         images = images + list(img_data)
-    mean_image = compute_mean_image(images)
-    return (images, mean_image)
+    #mean_image = compute_mean_image(images)
+    return (images)
 
 def show_image(image_vector):
     arr = np.array(image_vector)
     redarr = arr[0:1024]
     greenarr = arr[1024:2048]
     bluearr = arr[2048:]
-    # print("red array {}".format(redarr.shape))
-    # print("green array {}".format(greenarr.shape))
-    # print("blue array {}".format(bluearr.shape))
     rgbArray = np.zeros((32,32,3), 'uint8')
     rgbArray[..., 0] = redarr.reshape((32,-1))
     rgbArray[..., 1] = greenarr.reshape((32,-1))
@@ -72,20 +64,38 @@ if __name__ == "__main__":
     print(" ##### AML HW2 SVM Classifier  ##### ")
     datafiles = ["./cifar-10-batches-py/data_batch_1","./cifar-10-batches-py/data_batch_2","./cifar-10-batches-py/data_batch_3","./cifar-10-batches-py/data_batch_4","./cifar-10-batches-py/data_batch_5","./cifar-10-batches-py/test_batch"]
     image_means = []
-    for i in range(0,10):
+    image_files_by_category = []
+
+    pca = PCA(n_components=20, svd_solver='auto')
+    for i in range(0,1):
         #load all images of this class
-        images, mean_image = get_class_images_from_files(datafiles, i)
-        print("mean images for class {} is {}".format(i, mean_image))
-        show_image(mean_image)
+        images = get_class_images_from_files(datafiles, i)
         print("Found {} images of class {}".format(len(images), i))
+        # show_image(mean_image)
+        image_files_by_category.append(images)
         print("")
 
-    data1 = unpickle("./cifar-10-batches-py/data_batch_1")
-    # print(data1.keys())
-    filenames =data1[b'filenames']
-    labels= data1[b'labels']
-    print("First image label {}".format(labels[0]))
-    first_image = data1[b'data'][0]
+        pca.fit(np.array(images))
+        print("variance ratios {}".format(pca.explained_variance_ratio_)) 
+        print("singular values {}".format(pca.singular_values_))   
+        print("components {}".format(pca.components_))
+        print("mean_ {}".format(pca.mean_))
+        mean_image = pca.mean_
 
-    show_image(first_image)
+        print("###transforming images")
+        reduced_images = pca.fit_transform(images)
+        print("we got back {} images".format(reduced_images.shape))
+        print("reduced_image[0] {}".format(reduced_images[0]))
+        image_means.append(mean_image)
+
+
+
+    # data1 = unpickle("./cifar-10-batches-py/data_batch_1")
+    # # print(data1.keys())
+    # filenames =data1[b'filenames']
+    # labels= data1[b'labels']
+    # print("First image label {}".format(labels[0]))
+    # first_image = data1[b'data'][0]
+
+    # show_image(first_image)
     
