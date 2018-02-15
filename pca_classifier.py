007abd9b38144f79ccafabf01b34870bda1bbf0f
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-02-15 12:10:17 -0600

    fixed image reduction

--- pca_classifier.py
+++ pca_classifier.py
@@ -65,37 +65,50 @@ def compute_eigens(pca):
         print("EigenValue: {}".format(eigenvalue))
         print("Computed EigenValue: {}".format(np.dot(eigenvector.T, np.dot(pca.get_covariance(), eigenvector))))
 
-def compute_error(pca):
-    accuracy = 0.0
-    for val in pca.explained_variance_ratio_:
-        accuracy+=val
-    print("Error {}".format(1-accuracy))
+# def compute_error_method1(pca):
+#     accuracy = 0.0
+#     # print("explained variances {}".format(pca.explained_variance_))
+#     # print("explained variance ratios {}".format(pca.explained_variance_ratio_))
+#     for val in pca.explained_variance_ratio_:
+#         accuracy+=val
+#     print("Error of {} using Method 1".format(1-accuracy))
+
+def compute_error_method2(images, reduced_images, class_id):
+    print("Calculating Errors using Method 2...")
+
+    error_vector = np.array(images) - np.array(reduced_images)
+    N =len(error_vector)
+    squares = []
+    print("Computing squares of errors...")
+    for i in range(0, len(error_vector)):
+        squares.append(list(map(lambda x: x*x, error_vector[i])))
+    print("Computing sums of squares...")
+    sums = list(map(lambda x: sum(x), squares))
+    error = sum(sums)/N
+    print("Found mean error of {} for class_id {} using Method 2".format(error, class_id))
+    return error
 
 def calculate_reduced_images(images, n_components=20):
     pca = PCA(n_components)
     pca.fit(np.array(images))
-    # compute_eigens(pca)
-    compute_error(pca)
     mean_image = pca.mean_
     eigenvectors = pca.components_
-    print("eigenvectors.shape {}".format(eigenvectors.shape))
-    # eigenvalues = pca.explained_variance_
-
     diffs = images - mean_image
     
-    reduced_images = []
-    print("N {}".format(np.array(images).shape[0]))
-    for i in range(0, len(images)):
-        adjustment = 0
-        for j in range(n_components):
-            u = eigenvectors[j]
-            adjustment += u.T*diffs[i]*u 
-        reduced_images.append(mean_image + adjustment)
-        
-    print("reduced_images.shape {}".format(np.array(reduced_images).shape))
-    # print("diffs {}".format(diffs))
+    # reduced_images = []
+    # for i in range(0, len(images)):
+    #     adjustment = 0
+    #     for j in range(0, n_components):
+    #         u = eigenvectors[j]
+    #         adjustment += u.T*diffs[i]*u 
+    #     reduced_images.append(mean_image + adjustment)
 
-    return reduced_images, mean_image
+    transformed = pca.transform(images)
+    print("transformed.shape {}".format(transformed.shape))
+    inverse_transformed = pca.inverse_transform(transformed)
+    print("inverse_transformed.shape {}".format(inverse_transformed.shape))
+
+    return inverse_transformed, mean_image
 
 #main entry
 if __name__ == "__main__":
@@ -107,20 +120,30 @@ if __name__ == "__main__":
     # pca = PCA(n_components=20)
     for i in range(0,1):
         #load all images of this class
+        print(" ")
+        print("### Processing class {}".format(i))
         images = get_class_images_from_files(datafiles, i)
         print("Found {} images of class {}".format(len(images), i))
         # show_image(mean_image)
         image_files_by_category.append(images)
-        print("")
+        # print("")
 
-        reduced_images, mean_image = calculate_reduced_images(images, 20)
+        print("Generating Reduced Images...")
+        reduced_images, mean_image = calculate_reduced_images(images, 3072)
+        print("Mean image[:10] {}".format(mean_image[:10]))
 
-        error_images = np.array(images) - np.array(reduced_images)
+        # man_mean_image = compute_mean_image(images)
+        # print("Manual Mean image[:10] {}".format(man_mean_image[:10]))
+        # print("Generated {} Reduced Images of class {}".format(len(reduced_images), i))
+        # print("Calculating Errors...")
+        # error_images = np.array(images) - np.array(reduced_images)
+        compute_error_method2(images, reduced_images, i)
 
-        show_image(images[10])
-        show_image(reduced_images[10])
-        show_image(error_images[10])
-        show_image(mean_image)
+
+        show_image(images[0])
+        show_image(reduced_images[0])
+        # show_image(error_images[10])
+        # show_image(mean_image)
 
 
 
